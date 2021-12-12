5fddebc2cf5c7b8263d2cfdf1d58868c37dd89a1
Moskvichev <beldetal@yandex.ru>
2021-12-12 17:29:36 +0300

    finish

---
+++ gb-GU_Java_1154-master/server/src/main/java/ru/gb/mall/inventory/entity/Role.java
@@ -0,0 +1,18 @@
+package ru.gb.mall.inventory.entity;
+
+import lombok.Data;
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+}
