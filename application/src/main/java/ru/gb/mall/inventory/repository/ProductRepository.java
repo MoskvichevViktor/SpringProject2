3ef49a3a18ee913f2f2e56615787aa5870683ca3
Moskvichev <beldetal@yandex.ru>
2021-12-23 19:51:51 +0300

    end

--- application/src/main/java/ru/gb/mall/inventory/repository/ProductRepository.java
+++ application/src/main/java/ru/gb/mall/inventory/repository/ProductRepository.java
@@ -3,12 +3,10 @@ package ru.gb.mall.inventory.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.gb.mall.inventory.entity.Product;
 
 import java.awt.print.Pageable;
-import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
